8824da31820ea15efeee309ade7600d4eb59e034
RajMac <raj@RajsMacBook-Pro.local>
2019-03-28 11:17:06 -0700

    this is firt test in git demo repo

---
+++ src/jenkinsdemos/DemoJenkinsJobs.java
@@ -0,0 +1,16 @@
+package jenkinsdemos;
+import org.testng.annotations.Test;
+
+public class DemoJenkinsJobs {
+	
+	@Test
+	public void testJenkins() {
+		
+		
+		
+		System.out.println("  Welcome to Jenkins Demo World!  ");
+		
+		
+	}
+
+}
